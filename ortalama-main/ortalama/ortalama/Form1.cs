e33b924c75026e3c4483bf7b5c406ff6be393417
ssedeff <145463803+ssedeff@users.noreply.github.com>
2023-10-17 16:17:47 +0300

    Add files via upload

---
+++ ortalama-main/ortalama/ortalama/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ortalama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float yazili1, yazili2, ortalama;
+            yazili1=Convert.ToInt32(textBox1.Text);
+            yazili2=Convert.ToInt32(textBox2.Text);
+            ortalama = (yazili1 + yazili2) / 2;
+            label3.Text=ortalama.ToString();
+        }
+    }
+}
